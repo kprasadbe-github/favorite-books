0eb23331a6e2dea781fd953a5b6e3fcabb1efea0
prasadkb <prasad@auctionsoftware.com>
2016-09-19 22:25:38 +0530

    message

--- task/common.js
+++ task/common.js
@@ -6,7 +6,8 @@ exports.getSearchBooksData = function(req,mysql)
 	    {
 	    	req.query['search'] = '';
 	    }	
-	    var escape_data = [parseInt(req.session.userid)];
+	    var uid = (typeof(req.session.userid) == 'undefined') ? 0 : req.session.userid;
+	    var escape_data = [parseInt(uid)];
 		var strQuery = 'SELECT b.*,CONCAT(b.description,u.email,b.name,b.authour,b.published) AS data,u.email,(SELECT COUNT(id) FROM watchlist as w WHERE w.user_id = ? AND w.book_id = b.id) As watchlist FROM book AS b INNER JOIN users AS u  ON u.id = b.user_id  order by b.id desc';
 	    var defered = q.defer();
 		mysql.query(strQuery,escape_data,defered.makeNodeResolver());
